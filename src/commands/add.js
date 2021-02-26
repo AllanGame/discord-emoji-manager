e6a6ac6f85c5c8594ca04c19c4d6602495f1ba0f
AllanGame <allangameyt28@gmail.com>
2021-02-25 22:19:32 -0500

    Stable version

--- src/commands/add.js
+++ src/commands/add.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: "add",
     usage: "add <name> <url || attachment>",
-    alias: [],
+    alias: ["agregar"],
     onlyowner: false,
     onlydev: false,
     cooldown: 3,
@@ -13,51 +13,123 @@ module.exports = {
          * TODO: Add multiple emojis support [arg 1 must be "these"] - .zip files support
          */
 
-         if(!args[0]) return;
 
-        let emojiName = args[0];
-        let emojiURL = args[1];
-        if(message.attachments.size > 0){
-            emojiURL = message.attachments.first().url;
-        } 
+        /**
+         * COMMANDS: 
+         * !add <emoji> PLURAL: !add these <emojis>
+         * !add <name> <url> if there isnt name, return
+         * !add <name> ATTACHMENT if there isnt name, return
+         */
 
-            if(!args[1] && message.attachments.size < 0) {
-                emojiURL = args[0]
+
+
+         
+        // Adding multiple emojis
+        // !add these <emoji list>
+        if(args[0] === "these") {
+            let emojis = args.slice(1)
+            let msg = []
+            if(!args[1]) {
+                return message.channel.send('Please provide emojis to add!')
             }
-            let emojiTarget = Discord.Util.parseEmoji(emojiURL)
-                if(emojiTarget.id === null || emojiTarget.id === undefined) {
-                    if(message.attachments.size < 0) {
-                        return;
-                    }
-                    if(message.content.includes("https://cdn.discordapp.com")){
-                        return;
-                    }
-                    if(message.attachments.first().url.includes("https://cdn.discordapp.com/")) {
-                        return;
-                    }
-                    message.channel.send("`" + emojiURL + "`" + " isn't a valid emoji");
-                    return;
+
+            for(i=0; i<emojis.length; i++) {
+                let emoji = Discord.Util.parseEmoji(emojis[i]);
+                if(emoji.id === undefined || emoji.id === null) {
+                    message.channel.send("`" + emojis[i] + "` Isn't a valid emoji!")
+                    break;
                 }
-            if(emojiURL === args[0]) {
-             emojiName = emojiTarget.name;       
-            }
+                let emojiURL; 
+                let emojiExtention = ""
+                emoji.animated ? emojiExtention = ".gif" : emojiExtention = ".png"
+                emojiURL = "https://cdn.discordapp.com/emojis/"+emoji.id+emojiExtention+"?v=1";
+                message.guild.emojis.create(emojiURL, emoji.name)
+                .then(emoji => message.channel.send("Added: "+ emoji.toString()))
+                .catch(error => {
+                    if(error.code === 30008) {
+                        message.channel.send(error.message)
+                    }
+                });                
 
-            let emojiExtention = ""
-            if(emojiTarget.animated ? emojiExtention = ".gif" : emojiExtention = ".png");
-                emojiURL = "https://cdn.discordapp.com/emojis/"+emojiTarget.id+emojiExtention+"?v=1";
+            }
+            return;
+        }
+         // !add :emoji:
+         if(!args[2]) {
+             let emojiName;
+             let emojiURL; 
+             let emojiExtention = "";
+             if(!args[0]) {
+                 return message.channel.send('Please provide an emoji!');
+             }
+             let emoji = Discord.Util.parseEmoji(args[0]);
+             if(emoji.id === undefined || emoji.id === null) {
+                 return message.channel.send("Invalid emoji! \n" + "emoji id: "+ emoji.id);
+             }
+             if(args[1] && message.embeds.length <= 0) {
+                 emojiName = args[1];
+             } else {
+                 emojiName = emoji.name;
+             }
+
+            emoji.animated ? emojiExtention = ".gif" : emojiExtention = ".png";
+            emojiURL = "https://cdn.discordapp.com/emojis/"+emoji.id+emojiExtention+"?v=1";
+
+            message.guild.emojis.create(emojiURL, emojiName)
+            .then(emoji => message.channel.send("Added: "+ emoji.toString()))
+            .catch(error => {
+                if(error.code === 30008) {
+                    message.channel.send(error.message)
+                }
+            });   
+            return;
+          } 
+
+        //  if(!args[0]) return;
+
+        // let emojiName = args[0];
+        // let emojiURL = args[1];
+        // if(message.attachments.size > 0){
+        //     emojiURL = message.attachments.first().url;
+        // } 
+
+        //     if(!args[1] && message.attachments.size < 0) {
+        //         emojiURL = args[0]
+        //     }
+        //     let emojiTarget = Discord.Util.parseEmoji(emojiURL)
+        //         if(emojiTarget.id === null || emojiTarget.id === undefined) {
+        //             if(message.attachments.size < 0) {
+        //                 return;
+        //             }
+        //             if(message.content.includes("https://cdn.discordapp.com")){
+        //                 return;
+        //             }
+        //             if(message.attachments.first().url.includes("https://cdn.discordapp.com/")) {
+        //                 return;
+        //             }
+        //             message.channel.send("`" + emojiURL + "`" + " isn't a valid emoji");
+        //             return;
+        //         }
+        //     if(emojiURL === args[0]) {
+        //      emojiName = emojiTarget.name;       
+        //     }
+
+        //     let emojiExtention = ""
+        //     if(emojiTarget.animated ? emojiExtention = ".gif" : emojiExtention = ".png");
+        //         emojiURL = "https://cdn.discordapp.com/emojis/"+emojiTarget.id+emojiExtention+"?v=1";
  
 
-        if(!emojiName) {
-            return message.channel.send("Please provide a name")
-        }
-        if(!emojiURL) {
-            return message.channel.send("Please provide a emoji, you can provide a URL, emojis from other servers or attach an image/gif")
-        }
+        // if(!emojiName) {
+        //     return message.channel.send("Please provide a name")
+        // }
+        // if(!emojiURL) {
+        //     return message.channel.send("Please provide a emoji, you can provide a URL, emojis from other servers or attach an image/gif")
+        // }
 
 
-        message.channel.send(emojiURL)
-        message.channel.send(emojiName)
-        // message.guild.emojis.create(emojiURL, emojiName)
+        // message.channel.send(emojiURL)
+        // message.channel.send(emojiName)
+        // // message.guild.emojis.create(emojiURL, emojiName)
 
 
     }
---
+++ src/commands/remove.js
@@ -0,0 +1,65 @@
+module.exports = {
+    name: "remove",
+    usage: "remove <emoji>",
+    alias: [],
+    onlyowner: false,
+    onlydev: false,
+    cooldown: 3,
+    perms: ["MANAGE_EMOJIS"],
+    run: (client, message, args, storage) => {
+
+        /**
+         * COMMENT: This code sucks, but works 👍
+         */
+
+        const Discord = require("discord.js");
+
+        
+        if(!args[0]) {
+            return message.channel.send("Provide an emoji to remove")
+        }
+
+        let reason = args[1];
+        let emojiTarget;
+
+        if(!reason) {
+            reason = "removed by " + message.author.tag + " without reason"
+        }
+
+        let emojiID = message.guild.emojis.cache.get(args[0]);
+        if(emojiID !== undefined) {
+            emojiTarget = emojiID;
+            emojiTarget.delete(reason).then(r => {
+                message.channel.send(`The emoji was succesfully deleted with the reason \`${reason}\``)
+            })
+            return;
+        }
+
+        let emojiName = message.guild.emojis.cache.find(e => e.name === args[0]);
+        if(emojiName !== undefined) {
+            emojiTarget = emojiName;
+            emojiTarget.delete(reason).then(r => {
+                message.channel.send(`The emoji was succesfully deleted with the reason \`${reason}\``)
+            })
+            return;
+        }
+        let parseEmoji = Discord.Util.parseEmoji(args[0]);
+        if(parseEmoji.id !== undefined || parseEmoji.id !== null) {
+
+          if (client.emojis.cache.find((emoji) => emoji.id === parseEmoji.id) === undefined) {
+            return message.channel.send("that emoji isnt from this guild!");
+          }
+
+            emojiTarget = message.guild.emojis.cache.get(parseEmoji.id);
+            
+            emojiTarget.delete(reason).then(r => {
+                message.channel.send(`The emoji was succesfully deleted with the reason \`${reason}\``)
+            })
+            
+            
+        } else {
+            return message.channel.send("Invalid emoji")
+        }
+
+    }
+};
