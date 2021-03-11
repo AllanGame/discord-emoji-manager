1ef7c25a5b278f30262a8cf66fb6b5366f92e115
iAtog <atogmc58vani@gmail.com>
2021-03-11 16:19:52 -0430

    Now detects if the emoji is not a custom discord emoji

--- src/commands/info.js
+++ src/commands/info.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 module.exports = {
     name: "info",
     usage: "info <emoji>",
+    alias: ["emoji-info"],
     onlyowner: false,
     onlydev: false,
     cooldown: 2,
@@ -19,6 +20,14 @@ module.exports = {
 
         if (!args[0]) return message.reply(b("<:error:819654964628160527> Put the emoji or emoji id to get the data"));
         let emoji = args[0];
+        const ranges = [
+            '\ud83c[\udf00-\udfff]', // U+1F300 to U+1F3FF
+            '\ud83d[\udc00-\ude4f]', // U+1F400 to U+1F64F
+            '\ud83d[\ude80-\udeff]', // U+1F680 to U+1F6FF
+            ' ',
+          ].join('|');
+          const sc = s => s.replace(new RegExp(ranges, 'g'), '');
+          if(!sc(emoji).length)return message.channel.send(b("<:error:819654964628160527> The emoji you just inserted is a unicode emoji, currently I don't support these emojis, but soon I will!"));
         if (!has.test(emoji) && !client.emojis.cache.has(emoji)) return message.channel.send(b("<:error:819654964628160527> Invalid emoji."));
 
         const embed = new Discord.MessageEmbed()
@@ -30,17 +39,17 @@ module.exports = {
         if (!client.emojis.cache.has(id)) return message.channel.send(b("<:error:819654964628160527> This emoji does not exists"));
         let e = client.emojis.cache.get(id);
 
-        embed.setAuthor("Emoji: " + e.name, e.url)
+        embed.setAuthor("Emoji: " + e.name, "https://cdn.discordapp.com/emojis/818987710693900329.png?v=1")
         let roles = [];
         e.roles.cache.forEach(r => {
             roles.push("<@&" + r.id + ">");
         });
 
         embed.setDescription(`
-        **ID**: \`${e.id}\`
-        **<:gif:818988133269372960> Animated**: \`${e.animated ? 'Yes' : 'No'}\`
-        **Guild**: \`${e.guild.name}\`
-        **<:role:818987653118689360> Roles that can use this emoji**: ${roles.length < 1 ? '@everyone' : roles.join(", ")}
+        🆔 **ID**: \`${e.id}\`
+        <:gif:818988133269372960> **Animated**: \`${e.animated ? 'Yes' : 'No'}\`
+        <:discord_logo:818990574979514388> **Guild**: \`${e.guild.name}\`
+        <:role:818987653118689360> **Roles that can use this emoji**: ${roles.length < 1 ? '@everyone' : roles.join(", ")}
         `)
             .setThumbnail(e.url);
         message.channel.send(embed);
