77f7318a7aefd90d3da05996cf8b1f515a5de7d3
iAtog <atogmc58vani@gmail.com>
2021-03-12 19:49:38 -0430

    Sharding changes

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ const app = express();
 app.get('/', (req, res) => res.send(200));
 app.listen(3242);
 
-const { ShardingManager } = require('discord.js');
-const shards = new ShardingManager('./bot.js', {
+const { ShardingManager, MessageEmbed } = require('discord.js');
+const shards = new ShardingManager('./shard-bot.js', {
   token: require("./utils/data.json").token.discord,
   totalShards: 'auto',
   autoSpawn: true
@@ -13,3 +13,37 @@ const shards = new ShardingManager('./bot.js', {
 shards.on('shardCreate', shard => console.log(`[SHARD-${shard.id}] Executed... ${Math.floor(shard.id + 1)}/${shards.totalShards}`));
 
 shards.spawn();
+
+// Set the global functions
+
+/**
+ * 
+ * @param {String} text 
+ */
+global.b = function b(text) {
+    return new MessageEmbed().setDescription(text).setColor('RANDOM');
+}
+
+/**
+ * Fast way to create a embed
+ * @param {String} title 
+ * @param {String} description 
+ * @param {String} color 
+ * @param {Boolean} timestamp 
+ * @param {String[]} author 
+ * @param {String[]} footer 
+ * @param {String} image
+ * @param {String} thumbnail
+ */
+global.fe = function fe(title, description, color, timestamp, author, footer, image, thumbnail) {
+    const embed = new MessageEmbed();
+    if(title)embed.setTitle(title);
+    if(description)embed.setDescription(description);
+    if(color)embed.setColor(color);else embed.setColor('RANDOM');
+    if(timestamp)embed.setTimestamp(new Date());
+    if(author)embed.setAuthor(author[0], author[1], author[2]);
+    if(footer)embed.setFooter(footer[0], footer[1]);
+    if(image)embed.setImage(image);
+    if(thumbnail)embed.setThumbnail(thumbnail);
+    return embed;
+}
--- src/shard-bot.js
+++ src/shard-bot.js
@@ -7,33 +7,6 @@ let fs = require("fs");
 client.commands = new Discord.Collection();
 client.cooldowns = new Discord.Collection();
 
-global.b = function b(text) {
-    return new Discord.MessageEmbed().setDescription(text).setColor('RANDOM');
-}
-/**
- * Fast way to create a embed
- * @param {String} title 
- * @param {String} description 
- * @param {String} color 
- * @param {Boolean} timestamp 
- * @param {String[]} author 
- * @param {String[]} footer 
- * @param {String} image
- * @param {String} thumbnail
- */
-global.fe = function fe(title, description, color, timestamp, author, footer, image, thumbnail) {
-    const embed = new Discord.MessageEmbed();
-    if(title)embed.setTitle(title);
-    if(description)embed.setDescription(description);
-    if(color)embed.setColor(color);else embed.setColor('RANDOM');
-    if(timestamp)embed.setTimestamp(new Date());
-    if(author)embed.setAuthor(author[0], author[1], author[2]);
-    if(footer)embed.setFooter(footer[0], footer[1]);
-    if(image)embed.setImage(image);
-    if(thumbnail)embed.setThumbnail(thumbnail);
-    return embed;
-}
-
 fs.readdir(__dirname + "/commands", (err, files) => {
     if(err) {
         console.error(err);
@@ -46,12 +19,12 @@ fs.readdir(__dirname + "/commands", (err, files) => {
         return;
     }
 
-    console.log(`[INFO] Loading ${jsfiles.length} commands.`);
+    console.log(`[INFO] Loaded ${jsfiles.length} commands.`);
 
     jsfiles.forEach((f, i) => {
         let fileName = f.substring(0, f.length - 3);
         let fileContents = require("./commands/" + f);
-        console.log(`Command ${f} loaded`);
+        //console.log(`Command ${f} loaded`);
         client.commands.set(fileName, fileContents);
         delete require.cache[require.resolve(`./commands/${fileName}.js`)];
     });
