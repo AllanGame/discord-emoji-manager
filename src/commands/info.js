2a4924ed628aff2dc7f75f37cf6c025b807ebf20
AllanGame <allangameyt28@gmail.com>
2021-03-16 19:15:52 +0000

    ?

--- src/commands/info.js
+++ src/commands/info.js
@@ -18,16 +18,8 @@ module.exports = {
         const has = /<a?:.+:\d+>/gm;
         if (!args[0]) return message.inlineReply(b("<:error:819654964628160527> Put the emoji or emoji id to get the data"));
         let emoji = args[0];
-        const ranges = [
-            '\ud83c[\udf00-\udfff]', // U+1F300 to U+1F3FF
-            '\ud83d[\udc00-\ude4f]', // U+1F400 to U+1F64F
-            '\ud83d[\ude80-\udeff]', // U+1F680 to U+1F6FF
-            ' ',
-        ].join('|');
-        const sc = s => s.replace(new RegExp(ranges, 'g'), '');
-        //let name = emoji.split(':')[1];
-        //let id = emoji.split(':')[2].replace(">","");
-        if (!sc(emoji).length) return message.channel.send(b("<:error:819654964628160527> The emoji you just inserted is a unicode emoji, currently I don't support these emojis, but soon I will!"));
+        const emoji_regex = /^(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32-\ude3a]|[\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])+$/;
+        if (emoji_regex.test(emoji)) return message.channel.send(b("<:error:819654964628160527> The emoji you just inserted is a unicode emoji 😔, currently I don't support these emojis 😥, but soon I will! 😏"));
         if (!has.test(emoji) && !client.emojis.cache.find(x=>x.name==args[0])&& !client.emojis.cache.has(emoji)) return message.channel.send(b("<:error:819654964628160527> Invalid emoji."));
 
         const embed = new Discord.MessageEmbed()
