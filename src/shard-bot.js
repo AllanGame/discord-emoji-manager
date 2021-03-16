84f4379e6dc4d1a48902eae342eba1acde101b61
iAtog <atogmc58vani@gmail.com>
2021-03-15 21:56:19 -0430

    Ah, go!

--- src/shard-bot.js
+++ src/shard-bot.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js');
+const {MessageEmbed} = require('discord.js');
 const Client = require('./lib/Client');
 require('./lib/CStructure');
 const client = new Client();
