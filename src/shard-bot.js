66c0ea923e2ab42c821304d22411cc21d16bc71d
iAtog <atogmc58vani@gmail.com>
2021-03-15 21:47:46 -0430

    XD?

--- src/shard-bot.js
+++ src/shard-bot.js
@@ -59,3 +59,37 @@ client.login(data.token.discord).then(() => {
 }).catch((err) => {
     console.error(`[ERROR] Can't login. \n${err}`);
 });
+
+// Set the global functions
+
+/**
+ * 
+ * @param {String} text 
+ */
+global.b = function b(text) {
+    return new MessageEmbed().setDescription(text).setColor('RANDOM');
+}
+
+/**
+ * Fast way to create a embed
+ * @param {String} title 
+ * @param {String} description 
+ * @param {String} color 
+ * @param {Boolean} timestamp 
+ * @param {String[]} author 
+ * @param {String[]} footer 
+ * @param {String} image
+ * @param {String} thumbnail
+ */
+global.fe = function fe(title, description, color, timestamp, author, footer, image, thumbnail) {
+    const embed = new MessageEmbed();
+    if(title)embed.setTitle(title);
+    if(description)embed.setDescription(description);
+    if(color)embed.setColor(color);else embed.setColor('RANDOM');
+    if(timestamp)embed.setTimestamp(new Date());
+    if(author)embed.setAuthor(author[0], author[1], !author[2] ? "" : author[2]);
+    if(footer)embed.setFooter(footer[0], footer[1]);
+    if(image)embed.setImage(image);
+    if(thumbnail)embed.setThumbnail(thumbnail);
+    return embed;
+}
