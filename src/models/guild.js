636feb10975807bb56ea27bbc1d9462c95cbf707
AllanGame <allangameyt28@gmail.com>
2021-03-30 13:18:30 -0500

    a?

--- src/models/guild.js
+++ src/models/guild.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const guildSchema = new mongoose.Schema({
     guildID: String,
     prefix: String,
-    modlogsChannel: String
+    modlogsChannel: String,
     sanctions: Number,
     mutedRole: String,
 });
