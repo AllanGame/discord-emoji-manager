346335a4a424cc6ed8cf64b4418195d54608df10
AllanGame <allangameyt28@gmail.com>
2021-03-30 13:19:54 -0500

    DbUtils, and changes in guild model

--- src/models/guild.js
+++ src/models/guild.js
@@ -3,9 +3,10 @@ const mongoose = require("mongoose");
 const guildSchema = new mongoose.Schema({
     guildID: String,
     prefix: String,
-    modlogsChannel: String,
-    sanctions: Number,
-    mutedRole: String,
+    config: {
+        useEmojiFromLibrary: Boolean,
+        evalCommandAllowed: Boolean
+    }
 });
 
 module.exports = mongoose.model("Guild", guildSchema);
